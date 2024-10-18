98153dc53b3996643e0ab14a4af49c4643f9484a
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2024-10-18 00:32:58 -0300

    new project Personal website

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: { HelloWorld },
+  data() {
+    return {
+      texto: 'carlos'
+    }
+  }
+}
+</script>
+
+<template>
+
+  <HelloWorld :msg="texto" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    name: 'HelloWoeld',
+    props: {
+      msg: {type: String, required: true}
+    }
+  }
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  
+</template>
+
+<style scoped>
+
+</style>
